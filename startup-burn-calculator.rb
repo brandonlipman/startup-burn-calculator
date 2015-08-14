831abf75cb1316013b63ddef0f72ad7f98e3dc1b
Brandon Lipman <brandonlipman@gmail.com>
2015-08-14 12:44:51 -0400

    Create startup-burn-calculator.rb

---
+++ startup-burn-calculator.rb
@@ -0,0 +1,20 @@
+puts "Welcome to Startup Burn Calculator!"
+puts "What is the name of your startup? "
+	name = gets.chomp
+
+puts "How much is your startup spending per month? "
+	spend_per_month = gets.to_i
+
+puts "How many employees? "
+	employee_count = gets.to_i
+
+puts "Do you have any money? (Yes or NO) "
+	q_money = gets.to_s
+	puts (q_money == "Yes") ? "You're Fucked" : "Okay"
+
+puts "How much money do you have? "
+	amount_money = gets.to_i
+puts " "
+puts "Burn Report for " + name + ": "
+puts "   Per Emloyee: $" + (spend_per_month / employee_count).to_s
+puts "   Number of Months Runway: " + (amount_money / spend_per_month).to_s + " months left until your dead."
